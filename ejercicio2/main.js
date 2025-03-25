4e4950436f8a1d287548d162a17d7c7ca2b5e1e8
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2025-03-25 12:49:59 +0100

    "mejoras" visuales

--- ejercicio2/main.js
+++ ejercicio2/main.js
@@ -4,24 +4,30 @@ Ahora realizaremos una petición a la PokeAPI, queremos mostrar al entrar en la 
  la magia estará en que cada vez que recargues la página, será un nuevo Pokemon dentro de la primera generación de Pokemon, es decir, del 1 al 151.
 Los Pokemon no solo tienen una imagen, si no que tendrán muchas, hay que hallar la manera de encontrar la que más os guste. */
 
-const getRandomId = (max) => Math.floor((Math.random() * max)+ 1);
-
+const getRandomId = (max) => Math.floor(Math.random() * max + 1);
 
 const getPokemon = async () => {
-    try {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${getRandomId(151)}/`);
-        const pokemon = await res.json();
-        paintPokemon(pokemon);
-        
-    } catch (error) {
-        alert(error);
-    }
-}
+  try {
+    const res = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${getRandomId(151)}/`
+    );
+    const pokemon = await res.json();
+    paintPokemon(pokemon);
+  } catch (error) {
+    alert(error);
+  }
+};
 
 const paintPokemon = (pokemon) => {
-    const pokemonImage =  document.querySelector("img");
-    pokemonImage.setAttribute("src", `${pokemon.sprites.front_shiny}`);
-    pokemonImage.setAttribute("alt", `${pokemon.name}`);
-}
+  const pokemonImage = document.querySelector("img");
+  pokemonImage.setAttribute("src", `${pokemon.sprites.front_default}`);
+  pokemonImage.setAttribute("alt", `${pokemon.name}`);
+};
+
+const change = document.querySelector("#randomPokemon");
+
+change.addEventListener("click", () => {
+  getPokemon();
+});
 
 getPokemon();
